131d013290105e419c3209e385c406b2a1f99035
ASHvetsov <ashvetsov@inno.tech>
2025-08-19 19:38:06 +0300

    parser work

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 
+//import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("cvhnchmcvvbmcvbncvb: " + text.length());
+                Math.random();
     }
 }
